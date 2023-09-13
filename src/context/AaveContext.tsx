64321495c754d12888f39fa88637f877a3e369e7
FazioNico <contact@nicolasfazio.ch>
2023-09-13 15:23:15 +0200

    refactor: use new state type to manage multichain pools

--- src/context/AaveContext.tsx
+++ src/context/AaveContext.tsx
@@ -25,6 +25,9 @@ import {
 } from "../servcies/aave.service";
 import { useCurrentTimestamp } from "../hooks/useCurrentTimestamp";
 import dayjs from "dayjs";
+import { CHAIN_AVAILABLES } from "../constants/chains";
+import { IPoolGroup, IReserve } from "../interfaces/reserve.interface";
+import { getAssetIconUrl } from "../utils/getAssetIconUrl";
 
 export type ComputedReserveData = ReturnType<
   typeof formatReservesAndIncentives
@@ -48,10 +51,11 @@ const stub = (): never => {
   throw new Error("You forgot to wrap your component in <AaveProvider>.");
 };
 
-// Define the type for the user context.
+// Define the type for the AAVE context.
 type AaveContextType = {
-  markets: MARKETTYPE | null;
-  poolReserves: (ReserveDataHumanized & FormatReserveUSDResponse)[] | null;
+  markets: MARKETTYPE[] | null;
+  poolReserves: IReserve[] | null;
+  poolGroups: IPoolGroup[] | null;
   userSummary: FormatUserSummaryAndIncentivesResponse<
     ReserveDataHumanized & FormatReserveUSDResponse
   > | null;
@@ -63,6 +67,7 @@ type AaveContextType = {
 const AaveContextDefault: AaveContextType = {
   markets: null,
   poolReserves: null,
+  poolGroups: [],
   userSummary: null,
   totalTVL: null,
   userSummaryAndIncentives: null,
@@ -77,70 +82,109 @@ export const useAave = () => useContext(AaveContext);
 
 // Provider component that wraps parts of the app that need user context.
 export const AaveProvider = ({ children }: { children: React.ReactNode }) => {
-  const { ethereumProvider } = useEthersProvider();
-  const { user } = useUser();
+  // const { user } = useUser();
   const currentTimestamp = dayjs().unix(); // useCurrentTimestamp(5);
   const [state, setState] = useState<AaveContextType>(AaveContextDefault);
 
   const init = async () => {
     console.log("[INFO] {{AaveProvider}} init context... ");
-    const updatedState: AaveContextType = {
-      totalTVL: await fetchTVL().catch((error) => {
-        console.error("[ERROR] {{AaveProvider}} fetchTVL: ", error);
-        return -1;
-      }),
-    } as AaveContextType;
-
-    if (ethereumProvider?.network?.chainId) {
-      updatedState.markets = getMarkets(ethereumProvider.network.chainId);;
-    } else {
-      updatedState.markets = null;
-    }
-
-    if (ethereumProvider && !user) {
-      updatedState.userSummary = null;
-      updatedState.userSummaryAndIncentives = null;
-    }
-
-    if (ethereumProvider && state.markets) {
-      updatedState.poolReserves = await getPools({
-        provider: ethereumProvider,
-        market: state.markets,
-        currentTimestamp,
-      })
-      .catch((error) => {
+    // load markets from all available chains
+    const markets = CHAIN_AVAILABLES
+      .filter(chain => !chain.testnet)
+      .map((chain) => getMarkets(chain.id));
+    // looad pools from all available chains
+    let poolReserves: IReserve[] = []
+    if (markets && markets.length > 0) {
+      console.log("[INFO] {{AaveProvider}} fetchPools... ");
+      const pools = await Promise.all(
+        markets.map((market) => getPools({ market, currentTimestamp }))
+      ).catch((error) => {
         console.error("[ERROR] {{AaveProvider}} fetchPools: ", error);
-        return null;
+        return [];
       });
-    } else {
-      updatedState.poolReserves = null;
+      console.log("[INFO] {{AaveProvider}} fetchPools done: ", {pools});      
+      poolReserves = pools.flat() as IReserve[];
     }
-    
-    if (ethereumProvider && state.markets && user) {
-      updatedState.userSummaryAndIncentives = await getUserSummaryAndIncentives({
-        provider: ethereumProvider,
-        market: state.markets,
-        user,
-        currentTimestamp,
-      })
-      .catch((error) => {
-        console.error("[ERROR] {{AaveProvider}} fetchUserSummary: ", error);
-        return null;
-      });
-    } else {
-      updatedState.userSummaryAndIncentives = null;
-    }
-
+    // groups poolReserves by symbol (e.g. DAI, USDC, USDT, ...)
+    const poolGroups: IPoolGroup[] = poolReserves.reduce((acc, reserve) => {
+      const symbol = reserve.symbol;
+      const chainId = +(reserve.id.split('-')[0]);
+      const borrowApy = reserve.borrowingEnabled ? Number(reserve.variableBorrowAPY) : 0;
+      const supplyApy = Number(reserve.supplyAPY);
+      /// TODO: implement balance value
+      const walletBalance = 0;
+      const supplyBalance = 0;
+      const borrowBalance = 0;
+      const newReserve: IReserve = {
+        ...reserve,
+        logo: getAssetIconUrl({symbol: reserve.symbol}),
+        chainId,
+        walletBalance,
+        supplyBalance,
+        borrowBalance,
+      };
+      const poolGroup = acc.find((r) => r.symbol === symbol);
+      if (poolGroup) {
+        // add reserve to existing reserves group
+        poolGroup.reserves.push(newReserve);
+        // check if chainId exist and add if not
+        if (!poolGroup.chainIds.includes(chainId)) {
+          poolGroup.chainIds.push(chainId);
+        }
+        // check if borrowApy is lower than topBorrowApy
+        if (borrowApy < poolGroup.topBorrowApy) {
+          poolGroup.topBorrowApy = borrowApy;
+        }
+        // check if supplyApy is higher than topSupplyApy
+        if (supplyApy > poolGroup.topSupplyApy) {
+          poolGroup.topSupplyApy = supplyApy;
+        }
+        // update totalBorrowBalance
+        poolGroup.totalBorrowBalance += Number(newReserve.borrowBalance);
+        // update totalSupplyBalance
+        poolGroup.totalSupplyBalance += Number(newReserve.supplyBalance);
+        // update totalWalletBalance
+        poolGroup.totalWalletBalance += Number(newReserve.walletBalance);
+        // update borrowingEnabled if is disable and newReserve is enabled
+        if (newReserve.borrowingEnabled && !poolGroup.borrowingEnabled) {  
+          poolGroup.borrowingEnabled = newReserve.borrowingEnabled;
+        }
+      } else {
+        const newGroup: IPoolGroup = {
+          reserves: [newReserve], 
+          symbol,
+          name: reserve.name,
+          chainIds: [chainId],
+          logo: getAssetIconUrl({symbol}),
+          topBorrowApy: borrowApy,
+          topSupplyApy: supplyApy,
+          borrowingEnabled: reserve.borrowingEnabled,
+          totalBorrowBalance: borrowBalance,
+          totalSupplyBalance: supplyBalance,
+          totalWalletBalance: walletBalance,
+        }
+        acc.push(newGroup);
+      }
+      return acc;            
+    }, [] as IPoolGroup[])
+    // sort by existing ballances then by name
+    .sort((a, b) => {
+      if (a.symbol < b.symbol) return -1;
+      if (a.symbol > b.symbol) return 1;
+      return 0;
+    });
+    // update state
     setState((prev) => ({
       ...prev,
-      ...updatedState,
+      markets,
+      poolReserves,
+      poolGroups,
     }));
   };
 
   useEffect(() => {
     init();
-    return () => {};
-  }, [ethereumProvider?.network?.chainId, user, state.markets]);
+  }, []);
 
   return (
     <AaveContext.Provider
