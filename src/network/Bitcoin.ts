73b300c6c846b58e7e75d5bdb054ccfbbe51be4b
FazioNico <contact@nicolasfazio.ch>
2023-12-07 13:00:18 +0100

    refactor: reflect class change

--- src/network/Bitcoin.ts
+++ src/network/Bitcoin.ts
@@ -7,12 +7,15 @@ import { RPC_NODE_OPTIONS, getMagic } from "@/servcies/magic";
 export class BitcoinWalletUtils extends MagicWalletUtils { 
   
   public web3Provider: null = null;
+  public isMagicWallet = true;
+
   constructor(network: NETWORK) {
     super();
     this.network = network;
   }
 
   async _initializeWeb3() {
+    console.log(`[INFO] Bitcoin: initializeWeb3...`);
     const magic = await getMagic({chainId: this.network});
     const RPC_NODE = RPC_NODE_OPTIONS.find((n) => n.chainId === this.network);
     if (!RPC_NODE) {
