3ff53c59ebb5f3a8433b399996aa0931754ecaf7
FazioNico <contact@nicolasfazio.ch>
2023-12-07 12:56:42 +0100

    refactor: use new component to manage address over networks

--- src/components/AuthBadge.tsx
+++ src/components/AuthBadge.tsx
@@ -7,14 +7,22 @@ import {
   IonIcon,
   IonItem,
   IonLabel,
+  IonList,
   IonListHeader,
   IonRow,
+  IonSelect,
+  IonSelectOption,
   IonText,
   useIonAlert,
   useIonModal,
   useIonToast,
 } from "@ionic/react";
-import { copyOutline, checkmark, checkmarkCircleOutline, closeSharp } from "ionicons/icons";
+import {
+  copyOutline,
+  checkmark,
+  checkmarkCircleOutline,
+  closeSharp,
+} from "ionicons/icons";
 import { useEffect, useState } from "react";
 import { getAvatarFromEVMAddress } from "../servcies/avatar";
 import { getSplitedAddress } from "../utils/getSplitedAddress";
@@ -22,70 +30,68 @@ import DisconnectButton from "./DisconnectButton";
 import ShowUIButton from "./ShowUIButton";
 import { useWeb3Provider } from "../context/Web3Context";
 import { CHAIN_AVAILABLES, CHAIN_DEFAULT } from "@/constants/chains";
+import { SuccessCopyAddress } from "./SuccessCopyAddress";
+import { LoaderProvider, useLoader } from "@/context/LoaderContext";
+import { SelectNetwork } from "./SelectNetwork";
 
 export const AuthBadge: React.FC<any> = () => {
-  const { walletAddress, currentNetwork } = useWeb3Provider();
+  const { walletAddress, currentNetwork, switchNetwork, isMagicWallet } = useWeb3Provider();
+  const { display: displayLoader, hide: hidLoader } = useLoader();
   const [avatarUrl, setAvatarUrl] = useState("");
-  const chain = CHAIN_AVAILABLES.find((chain) => chain.id === currentNetwork) || CHAIN_DEFAULT;
-  const [present, dismiss] = useIonModal(()=> {
-    return (<>
-      <IonGrid>
-        <IonRow>
-          <IonCol className="ion-text-center ion-padding">
-          <IonButton
-            size="small"
-            fill="clear"
-            style={{ position: "absolute", top: "0.25rem", right: "0rem" }}
-            onClick={() => dismiss(null, "cancel")}
-          >
-            <IonIcon slot="icon-only" icon={closeSharp}></IonIcon>
-          </IonButton>
-            <IonText>
-              <IonIcon 
-              color="success" 
-              style={{
-                display: "block",
-                fontSize: "5rem",
-                margin: '1rem auto'
-              }} src={checkmarkCircleOutline} />
-              <h3>Address Copied</h3>
-            </IonText>
-            <IonText color="medium">
-              <p style={{fontSize: '60%'}}>
-                {walletAddress}
-              </p>
-            </IonText>
-            <IonText>
-              <p>Address successfuly copy to clipboard. 
-                Be careful, only transfer <b>{chain.name} Network</b> tokens to this address.</p>
-            </IonText>
-            <IonButton
-            color="gradient"
-            fill="solid"
-            expand="block"
-            style={{  margin: "1.5rem auto 0" }}
-            onClick={() => dismiss(null, "cancel")}
-          >
-            OK
-          </IonButton>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
-    </>)
-  });
-  const handleActions = async (type: string, payload: string) => {
-    if (type === "copy") {
-      navigator?.clipboard?.writeText(payload);
-      // display toast confirmation
-      await present({
-        cssClass: 'modalAlert',
-        // header: "Address Copied",
-        // subHeader: payload,
-        // message: `Full ${chain.name} Network address copy to clipboard. Be careful, only transfer ${chain.name} Network tokens to this address.`,
-        // buttons: ["OK"],
+  const chain =
+    CHAIN_AVAILABLES.find((chain) => chain.id === currentNetwork) ||
+    CHAIN_DEFAULT;
+  const [presentSuccessCopyAddress, dismissSuccessCopyAddress] = useIonModal(
+    () => (
+      <SuccessCopyAddress
+        walletAddress={walletAddress || ""}
+        chain={chain}
+        dismiss={dismissSuccessCopyAddress}
+      />
+    )
+  );
+  const [presentSelectNetwork, dismissSelectNetwork] = useIonModal(() => (
+    <SelectNetwork chains={CHAIN_AVAILABLES} isMagicWallet={isMagicWallet} dismiss={dismissSelectNetwork} />
+  ));
 
-      });
+  const handleActions = async (type: string, payload: string) => {
+    await displayLoader();
+    switch (true) {
+      case type === "copy": {
+        navigator?.clipboard?.writeText(payload);
+        // display toast confirmation
+        presentSuccessCopyAddress({
+          cssClass: "modalAlert",
+          onDidDismiss(event) {
+            console.log("onDidDismiss", event.detail.role);
+            if (!event.detail.role || event?.detail?.role === 'cancel') return;
+            handleActions(event.detail.role, payload);
+          },
+        });
+        break;
+      }
+      case type === "selectNetwork": {
+        presentSelectNetwork({
+          cssClass: "modalAlert",
+          onDidDismiss(event) {
+            if (!event.detail.role || event?.detail?.role === 'cancel') return;
+            handleActions(event.detail.role, event.detail.data).then(() =>
+              hidLoader()
+            );
+          },
+        });
+        break;
+      }
+      case type === "getAddressFromNetwork": {
+        await switchNetwork(Number(payload));
+        dismissSelectNetwork(null, "cancel");
+        await handleActions("copy", `${walletAddress}`);
+        break;
+      }
+      default:
+        break;
     }
+    await hidLoader();
   };
 
   useEffect(() => {
@@ -111,7 +117,11 @@ export const AuthBadge: React.FC<any> = () => {
       <IonListHeader>
         <IonLabel>Wallet</IonLabel>
       </IonListHeader>
-      <IonItem className="ion-margin-vertical" lines="none" style={{"--background": "transparent"}}>
+      <IonItem
+        className="ion-margin-vertical"
+        lines="none"
+        style={{ "--background": "transparent" }}
+      >
         <IonAvatar slot="start">
           <img src={avatarUrl} alt="avatar" />
         </IonAvatar>
@@ -120,13 +130,16 @@ export const AuthBadge: React.FC<any> = () => {
             <small>{chain.name} Network</small>
           </IonText>
           <IonText color="success">
-            <IonIcon style={{
-                  display: "inline-block",
-                  verticalAlign: "top",
-                  marginRight: "0.25rem"
-            }} src={checkmark}></IonIcon>
+            <IonIcon
+              style={{
+                display: "inline-block",
+                verticalAlign: "top",
+                marginRight: "0.25rem",
+              }}
+              src={checkmark}
+            ></IonIcon>
             Connected
-            </IonText>
+          </IonText>
         </IonLabel>
         <IonIcon
           size="small"
