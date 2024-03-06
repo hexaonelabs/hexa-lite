c5d02a557e13b6d9bcba7bd990416a2dfa5215d7
FazioNico <contact@nicolasfazio.ch>
2024-03-06 12:48:22 +0100

    refactor: Add isMobilePWADevice check for mobile UI

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -20,6 +20,7 @@ import { getMagic } from '@/servcies/magic';
 import Store from '@/store';
 import { getWeb3State } from '@/store/selectors';
 import { useIonRouter, IonRoute } from '@ionic/react';
+import { isPlatform } from '@ionic/core';
 
 setupIonicReact({ mode: 'ios' });
 
@@ -36,9 +37,10 @@ const SwapContainer = lazy(() => import('@/containers/SwapContainer'));
 const DefiContainer = lazy(() => import('@/containers/DefiContainer'));
 const EarnContainer = lazy(() => import('@/containers/EarnContainer'));
 
+const isMobilePWADevice = Boolean(isPlatform('pwa') && !isPlatform('desktop'));
+
 const AppShell = () => {
   // get params from url `s=`
-  const urlParams = new URLSearchParams(window.location.search);
   const {pathname = '/swap'} = window.location;
   let segment = pathname.split('/')[1]|| 'swap' // urlParams.get("s") || "swap";
   const {walletAddress, isMagicWallet } = Store.useState(getWeb3State);
@@ -76,58 +78,67 @@ const AppShell = () => {
   
   return (
     <IonApp>
-      <IonReactRouter >
-        <IonRouterOutlet id="main">
-          <IonRoute path="/index" render={() => <>
-            <IonPage>
-              <IonContent ref={contentRef} scrollEvents={true} >
-                <IonGrid class="ion-no-padding" style={{ minHeight: "100vh" }}>
-                  <IonRow
-                    style={{
-                      minHeight: "100%",
-                      height: currentSegment !== "welcome" ? "100%" : "90vh",
-                    }}
-                    class={
-                      currentSegment !== "welcome"
-                        ? "ion-align-items-top ion-justify-content-center ion-no-padding"
-                        : "ion-align-items-center ion-justify-content-center ion-no-padding"
-                    }
-                  >
-                    <IonCol size="12" class="ion-no-padding">
-                    <Welcome handleSegmentChange={handleSegmentChange} />
-                    </IonCol>
-                  </IonRow>
-                </IonGrid>
-              </IonContent>
-            </IonPage>
-          </>} />
-          <IonRoute path="/leaderboard" render={() => <Leaderboard />} />
-          <IonRoute path="/" render={() => <Redirect to="/index" />} exact={true} />
-          <IonRoute render={() => <>
-            <IonPage>
-              {!isNotFound && (
-                <Header 
-                  currentSegment={currentSegment} 
-                  handleSegmentChange={handleSegmentChange} />
-              )}
-              <IonContent>
-                <Suspense fallback={<div>Loading SwapContainer...</div>}>
-                  { currentSegment === 'swap' && <SwapContainer />}
-                </Suspense>
-                <Suspense fallback={<div>Loading EarnContainer...</div>}>
-                  { currentSegment === 'earn' && <EarnContainer />}
-                </Suspense>
-                <Suspense fallback={<div>Loading DefiContainer...</div>}>
-                  { currentSegment === 'defi' && <DefiContainer handleSegmentChange={handleSegmentChange} />}
-                </Suspense>
-                <Suspense fallback={<div>Loading NotFoundPage...</div>}>
-                  { currentSegment === isNotFound && <NotFoundPage />}
-                </Suspense>
-              </IonContent>
-            </IonPage>
-          </>} />
-        </IonRouterOutlet>
-      </IonReactRouter>
+      {!isMobilePWADevice && (
+        <IonReactRouter >
+          <IonRouterOutlet id="main">
+            <IonRoute path="/index" render={() => <>
+              <IonPage>
+                <IonContent ref={contentRef} scrollEvents={true} >
+                  <IonGrid class="ion-no-padding" style={{ minHeight: "100vh" }}>
+                    <IonRow
+                      style={{
+                        minHeight: "100%",
+                        height: currentSegment !== "welcome" ? "100%" : "90vh",
+                      }}
+                      class={
+                        currentSegment !== "welcome"
+                          ? "ion-align-items-top ion-justify-content-center ion-no-padding"
+                          : "ion-align-items-center ion-justify-content-center ion-no-padding"
+                      }
+                    >
+                      <IonCol size="12" class="ion-no-padding">
+                      <Welcome handleSegmentChange={handleSegmentChange} />
+                      </IonCol>
+                    </IonRow>
+                  </IonGrid>
+                </IonContent>
+              </IonPage>
+            </>} />
+            <IonRoute path="/leaderboard" render={() => <Leaderboard />} />
+            <IonRoute path="/" render={() => <Redirect to="/index" />} exact={true} />
+            <IonRoute render={() => <>
+              <IonPage>
+                {!isNotFound && (
+                  <Header 
+                    currentSegment={currentSegment} 
+                    handleSegmentChange={handleSegmentChange} />
+                )}
+                <IonContent>
+                  <Suspense fallback={<div>Loading SwapContainer...</div>}>
+                    { currentSegment === 'swap' && <SwapContainer />}
+                  </Suspense>
+                  <Suspense fallback={<div>Loading EarnContainer...</div>}>
+                    { currentSegment === 'earn' && <EarnContainer />}
+                  </Suspense>
+                  <Suspense fallback={<div>Loading DefiContainer...</div>}>
+                    { currentSegment === 'defi' && <DefiContainer handleSegmentChange={handleSegmentChange} />}
+                  </Suspense>
+                  <Suspense fallback={<div>Loading NotFoundPage...</div>}>
+                    { currentSegment === isNotFound && <NotFoundPage />}
+                  </Suspense>
+                </IonContent>
+              </IonPage>
+            </>} />
+          </IonRouterOutlet>
+        </IonReactRouter>
+      )}
+      {isMobilePWADevice && (
+        <IonPage>
+          <IonContent>
+            {/* Here use mobile UI */}
+          </IonContent>
+        </IonPage>
+      )}
       <PwaInstall />
     </IonApp>
   );
