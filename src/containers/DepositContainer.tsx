6aab7a00d21e7f45fbbfd474d5753e3ee81bb319
FazioNico <contact@nicolasfazio.ch>
2024-03-18 11:47:10 +0100

    feat: add copy address and switch network features

--- src/containers/DepositContainer.tsx
+++ src/containers/DepositContainer.tsx
@@ -1,4 +1,4 @@
-import { CHAIN_AVAILABLES } from "@/constants/chains";
+import { CHAIN_AVAILABLES, CHAIN_DEFAULT } from "@/constants/chains";
 import { getQrcodeAsSVG } from "@/servcies/qrcode.service";
 import Store from "@/store";
 import { getWeb3State } from "@/store/selectors";
@@ -13,20 +13,82 @@ import {
   IonRow,
   IonText,
   IonToolbar,
+  useIonModal,
 } from "@ionic/react";
 import { useEffect, useState } from "react";
-import { scan } from 'ionicons/icons';
+import { copyOutline, scan } from 'ionicons/icons';
+import { SuccessCopyAddress } from "@/components/SuccessCopyAddress";
+import { useLoader } from "@/context/LoaderContext";
+import { SelectNetwork } from "@/components/SelectNetwork";
 
 export const DepositContainer = () => {
   const {
-    web3Provider,
     currentNetwork,
     walletAddress,
-    connectWallet,
-    disconnectWallet,
     switchNetwork,
+    isMagicWallet
   } = Store.useState(getWeb3State);
   const [qrCodeSVG, setQrCodeSVG] = useState<SVGElement | null>(null);
+  const chain =
+    CHAIN_AVAILABLES.find((chain) => chain.id === currentNetwork) ||
+    CHAIN_DEFAULT;
+  const [presentSuccessCopyAddress, dismissSuccessCopyAddress] = useIonModal(
+    () => (
+      <SuccessCopyAddress
+        walletAddress={walletAddress || ""}
+        chain={chain}
+        dismiss={dismissSuccessCopyAddress}
+      />
+    )
+  );
+  const [presentSelectNetwork, dismissSelectNetwork] = useIonModal(() => (
+    <SelectNetwork
+      chains={CHAIN_AVAILABLES}
+      isMagicWallet={isMagicWallet}
+      dismiss={dismissSelectNetwork}
+    />
+  ));
+  const { display: displayLoader, hide: hidLoader } = useLoader();
+
+  const handleActions = async (type: string, payload: string) => {
+    await displayLoader();
+    switch (true) {
+      case type === "copy": {
+        navigator?.clipboard?.writeText(payload);
+        // display toast confirmation
+        presentSuccessCopyAddress({
+          cssClass: "modalAlert",
+          onDidDismiss(event) {
+            console.log("onDidDismiss", event.detail.role);
+            if (!event.detail.role || event?.detail?.role === "cancel") return;
+            handleActions(event.detail.role, payload);
+          },
+        });
+        break;
+      }
+      case type === "selectNetwork": {
+        presentSelectNetwork({
+          cssClass: "modalAlert",
+          onDidDismiss(event) {
+            if (!event.detail.role || event?.detail?.role === "cancel") return;
+            handleActions(event.detail.role, event.detail.data).then(() =>
+              hidLoader()
+            );
+          },
+        });
+        break;
+      }
+      case type === "getAddressFromNetwork": {
+        await switchNetwork(Number(payload));
+        dismissSelectNetwork(null, "cancel");
+        await handleActions("copy", `${walletAddress}`);
+        break;
+      }
+      default:
+        break;
+    }
+    await hidLoader();
+  };
 
   useEffect(() => {
     if (!walletAddress) {
@@ -60,10 +122,18 @@ export const DepositContainer = () => {
       </IonHeader>
       <IonContent className="mobileConentModal">
             <IonText color="medium">
-                <p className="ion-text-center">
-                  <small>
-                    {walletAddress}
-                  </small>
+                <p 
+                  className="ion-text-center">
+                  Wallet Address<br/>
+                  <IonText color="primary">
+                    <small onClick={() => handleActions("copy", walletAddress || "")} style={{ cursor: "pointer" }}>
+                      {walletAddress?.slice(0, 6)}...{walletAddress?.slice(walletAddress.length - 6, walletAddress.length)}
+                      <IonIcon
+                        icon={copyOutline}
+                        style={{ fontSize: '0.65rem', marginLeft: '0.25rem' }} />
+                    </small>
+
+                  </IonText>
                 </p>
               </IonText>
         <IonGrid className="ion-margin-bottom ion-padding-bottom">
