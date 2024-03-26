eff146f4baa8c67f4c410b53c2532bd6b3c3355f
FazioNico <contact@nicolasfazio.ch>
2024-03-27 00:40:20 +0100

    refactor: use third-parties from Nextjs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
- 
+import { GoogleAnalytics } from '@next/third-parties/google';
+
 export default function Document() {
   return (
     <Html lang="en" style={{background: '#182449'} }>
@@ -8,6 +9,7 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      <GoogleAnalytics gaId="G-70XCWQ9YE2" />
     </Html>
   )
 }
