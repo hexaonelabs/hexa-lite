89ed2966cc314cd72463d29d810b4d397fe7a5e8
FazioNico <contact@nicolasfazio.ch>
2024-04-16 20:46:43 +0200

    refactor: handle with debounce and warning msg

---
+++ src/components/ui/InputAssetWithDropDown/InputAssetWithDropDown.tsx
@@ -0,0 +1,194 @@
+import { SymbolIcon } from "@/components/SymbolIcon";
+import { CHAIN_AVAILABLES, CHAIN_DEFAULT } from "@/constants/chains";
+import { IAsset } from "@/interfaces/asset.interface";
+import { getReadableAmount } from "@/utils/getReadableAmount";
+import { InputInputEventDetail, IonInputCustomEvent } from "@ionic/core";
+import { IonCol, IonGrid, IonIcon, IonInput, IonItem, IonLabel, IonList, IonListHeader, IonModal, IonRow, IonText } from "@ionic/react";
+import { chevronDown } from "ionicons/icons";
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from "react";
+
+const isNumberKey = (evt: React.KeyboardEvent<HTMLIonInputElement>) => {
+  var charCode = evt.which ? evt.which : evt.keyCode;
+  return !(charCode > 31 && (charCode < 48 || charCode > 57));
+};
+
+export const InputAssetWithDropDown = (props: {
+  assets: IAsset[];
+  inputFromAmount: number;
+  setInputFromAmount: Dispatch<SetStateAction<number>>;
+  setInputFromAsset: Dispatch<SetStateAction<IAsset | undefined>>;
+}) => {
+  const { assets, setInputFromAmount, inputFromAmount, setInputFromAsset } =
+    props;
+  const [selectedAsset, setSelectedAsset] = useState(assets[0]);
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  
+  useEffect(() => {
+    if (selectedAsset) {
+      setInputFromAsset(selectedAsset);
+    }
+    return () => {};
+  });
+
+  const maxBalance = useMemo(() => {
+    // round to the lower tenth
+    return Math.floor(selectedAsset?.balance * 10000) / 10000;
+  }, [selectedAsset]);
+
+  const handleInputChange = async (
+    e: IonInputCustomEvent<InputInputEventDetail>
+  ) => {
+    let value = Number((e.target as any).value || 0);
+    setInputFromAmount(() => value);
+  };
+
+  return (
+    <>
+      <IonGrid className="ion-no-padding InputAssetWithDropDown">
+        <IonRow className="ion-align-items-center">
+          <IonCol size="auto">
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
+              onClick={($event) => {
+                $event.stopPropagation();
+                setPopoverOpen(() => true);
+              }}
+            >
+              <div
+                id="select-from"
+                style={{
+                  cursor: "pointer",
+                  display: "flex",
+                  flexDirection: "row",
+                  flexWrap: "nowrap",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  alignContent: "flex-start",
+                }}
+              >
+                <IonIcon src={chevronDown} style={{ marginRight: "0.25rem" }} />
+                <SymbolIcon
+                  assetIconURL={
+                    selectedAsset?.symbol === "ETH"
+                      ? undefined
+                      : selectedAsset.thumbnail
+                  }
+                  symbol={selectedAsset?.symbol || ""}
+                  chainId={selectedAsset?.chain?.id || CHAIN_DEFAULT.id}
+                  iconSize="34px"
+                />
+              </div>
+
+              <div className="ion-padding" style={{ cursor: "pointer" }}>
+                <IonText>
+                  <h3 style={{ margin: " 0" }}>{selectedAsset?.symbol}</h3>
+                </IonText>
+                <IonText
+                  color="medium"
+                  onClick={($event) => {
+                    $event.stopPropagation();
+                    setInputFromAmount(() => selectedAsset?.balance || 0);
+                  }}
+                >
+                  <small style={{ margin: "0" }}>Max: {maxBalance}</small>
+                </IonText>
+              </div>
+            </div>
+          </IonCol>
+          <IonCol>
+            <div className="ion-text-end" style={{marginRight: '16px', overflow: 'hidden'}}>
+              <IonInput
+                style={{ fontSize: "1.5rem", minWidth: "100px", transform: 'translateX(16px)' }}
+                placeholder="0"
+                type="number"
+                max={maxBalance}
+                min={0}
+                debounce={800}
+                value={inputFromAmount}
+                onIonInput={(e) => handleInputChange(e)}
+              />
+            </div>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+
+      <IonModal
+        isOpen={popoverOpen}
+        onDidDismiss={() => setPopoverOpen(false)}
+        className="modalAlert"
+      >
+        <IonListHeader>
+          <IonText>
+            <p>Available assets</p>
+          </IonText>
+        </IonListHeader>
+        <IonList
+          style={{ background: "transparent" }}
+          className="ion-padding-bottom"
+        >
+          {assets
+            .filter((a) => a.balance > 0)
+            .map((asset, index) => (
+              <IonItem
+                button
+                detail={false}
+                key={index}
+                style={{ "--background": "transparent" }}
+                onClick={() => {
+                  setPopoverOpen(() => false);
+                  setSelectedAsset(asset);
+                  setInputFromAsset(asset);
+                  setInputFromAmount(() => 0);
+                  console.log('[INFO] selectedAsset: ',{ selectedAsset });
+                }}
+              >
+                <div slot="start" style={{ margin: "0 0.25rem 0 0" }}>
+                  <SymbolIcon
+                    assetIconURL={
+                      asset?.symbol === "ETH" ? undefined : asset.thumbnail
+                    }
+                    symbol={asset.symbol}
+                    chainId={asset.chain?.id}
+                    iconSize="28px"
+                  />
+                </div>
+                <IonLabel
+                  style={{
+                    marginLeft: "0.25rem",
+                    lineHeight: "0.8rem",
+                  }}
+                >
+                  <IonText>{asset.symbol}</IonText>
+                  <br />
+                  <IonText color="medium">
+                    <small>
+                      {
+                        CHAIN_AVAILABLES.find((c) => c.id === asset?.chain?.id)
+                          ?.name
+                      }
+                    </small>
+                  </IonText>
+                </IonLabel>
+                <div slot="end" className="ion-text-end">
+                  <IonText>{Number(asset?.balance).toFixed(6)}</IonText>
+                  <br />
+                  <IonText color="medium">
+                    <small>
+                      {getReadableAmount(
+                        +asset?.balance,
+                        Number(asset?.priceUsd),
+                        "No deposit"
+                      )}
+                    </small>
+                  </IonText>
+                </div>
+              </IonItem>
+            ))}
+        </IonList>
+      </IonModal>
+    </>
+  );
+};
--- src/containers/TransferContainer.tsx
+++ src/containers/TransferContainer.tsx
@@ -12,38 +12,22 @@ import {
   IonHeader,
   IonIcon,
   IonInput,
-  IonItem,
   IonLabel,
-  IonList,
-  IonListHeader,
   IonModal,
   IonRow,
   IonSpinner,
-  IonText,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import { chevronDown, close, scan } from "ionicons/icons";
-import { SymbolIcon } from "../components/SymbolIcon";
-import {
-  Dispatch,
-  SetStateAction,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
-import { CHAIN_AVAILABLES, CHAIN_DEFAULT } from "@/constants/chains";
-import { getReadableAmount } from "@/utils/getReadableAmount";
-import { InputInputEventDetail, IonInputCustomEvent } from "@ionic/core";
+
+import { useEffect, useRef, useState } from "react";
+
 import { Html5Qrcode } from "html5-qrcode";
 import { CheckIcon } from "@/components/ui/CheckIcon/CheckIcon";
 import { CrossIcon } from "@/components/ui/CrossIcon/CrossIcon";
-
-const isNumberKey = (evt: React.KeyboardEvent<HTMLIonInputElement>) => {
-  var charCode = evt.which ? evt.which : evt.keyCode;
-  return !(charCode > 31 && (charCode < 48 || charCode > 57));
-};
+import { close, scan } from "ionicons/icons";
+import { InputAssetWithDropDown } from "@/components/ui/InputAssetWithDropDown/InputAssetWithDropDown";
+import { WarningBox } from "@/components/WarningBox";
 
 const scanQrCode = async (
   html5QrcodeScanner: Html5Qrcode
@@ -170,213 +154,6 @@ const ScanModal = (props: {
   );
 };
 
-const InputAssetWithDropDown = (props: {
-  assets: IAsset[];
-  inputFromAmount: number;
-  setInputFromAmount: Dispatch<SetStateAction<number>>;
-  setInputFromAsset: Dispatch<SetStateAction<IAsset | undefined>>;
-}) => {
-  const { assets, setInputFromAmount, inputFromAmount, setInputFromAsset } =
-    props;
-  const [errorMessage, setErrorMessage] = useState<string | undefined>();
-  const [selectedAsset, setSelectedAsset] = useState(assets[0]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [popoverOpen, setPopoverOpen] = useState(false);
-  // const popover = useRef<HTMLIonPopoverElement>(null);
-
-  useEffect(() => {
-    if (selectedAsset) {
-      setInputFromAsset(selectedAsset);
-    }
-    return () => {};
-  });
-
-  const maxBalance = useMemo(() => {
-    // round to the lower tenth
-    return Math.floor(selectedAsset?.balance * 10000) / 10000;
-  }, [selectedAsset]);
-
-  const handleInputChange = async (
-    e: IonInputCustomEvent<InputInputEventDetail>
-  ) => {
-    let value = Number((e.target as any).value || 0);
-    if (maxBalance && value > maxBalance) {
-      (e.target as any).value = maxBalance;
-      value = maxBalance;
-    }
-    if (value <= 0) {
-      setErrorMessage(() => undefined);
-      // UI loader control
-      setIsLoading(() => false);
-      return;
-    }
-    setInputFromAmount(() => value);
-    setErrorMessage(() => undefined);
-    // UI loader control
-    setIsLoading(() => false);
-  };
-
-  return (
-    <>
-      <IonGrid className="ion-no-padding InputAssetWithDropDown">
-        <IonRow className="ion-align-items-center">
-          <IonCol size="auto">
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-              onClick={($event) => {
-                $event.stopPropagation();
-                // set position
-                // popover.current!.event = $event;
-                // open popover
-                setPopoverOpen(() => true);
-              }}
-            >
-              <div
-                id="select-from"
-                style={{
-                  cursor: "pointer",
-                  display: "flex",
-                  flexDirection: "row",
-                  flexWrap: "nowrap",
-                  justifyContent: "flex-start",
-                  alignItems: "center",
-                  alignContent: "flex-start",
-                }}
-              >
-                <IonIcon src={chevronDown} style={{ marginRight: "0.25rem" }} />
-                <SymbolIcon
-                  assetIconURL={
-                    selectedAsset?.symbol === "ETH"
-                      ? undefined
-                      : selectedAsset.thumbnail
-                  }
-                  symbol={selectedAsset?.symbol || ""}
-                  chainId={selectedAsset?.chain?.id || CHAIN_DEFAULT.id}
-                  iconSize="34px"
-                />
-              </div>
-
-              <div className="ion-padding" style={{ cursor: "pointer" }}>
-                <IonText>
-                  <h3 style={{ margin: " 0" }}>{selectedAsset?.symbol}</h3>
-                </IonText>
-                <IonText
-                  color="medium"
-                  onClick={($event) => {
-                    $event.stopPropagation();
-                    setInputFromAmount(() => selectedAsset?.balance || 0);
-                  }}
-                >
-                  <small style={{ margin: "0" }}>Max: {maxBalance}</small>
-                </IonText>
-              </div>
-            </div>
-          </IonCol>
-          <IonCol>
-            <div className="ion-text-end">
-              <IonInput
-                style={{ fontSize: "1.5rem", minWidth: "100px" }}
-                placeholder="0"
-                type="number"
-                max={maxBalance}
-                min={0}
-                debounce={1500}
-                value={inputFromAmount}
-                onKeyUp={(e) => {
-                  if (isNumberKey(e)) {
-                    setIsLoading(() => true);
-                  }
-                }}
-                onIonInput={(e) => handleInputChange(e)}
-              />
-            </div>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
-
-      <IonModal
-        isOpen={popoverOpen}
-        onDidDismiss={() => setPopoverOpen(false)}
-        className="modalAlert"
-      >
-        <IonListHeader>
-          <IonText>
-            <p>Available assets</p>
-          </IonText>
-        </IonListHeader>
-        <IonList
-          style={{ background: "transparent" }}
-          className="ion-padding-bottom"
-        >
-          {assets
-            .filter((a) => a.balance > 0)
-            .map((asset, index) => (
-              <IonItem
-                button
-                detail={false}
-                key={index}
-                style={{ "--background": "transparent" }}
-                onClick={() => {
-                  setPopoverOpen(() => false);
-                  setSelectedAsset(asset);
-                  setInputFromAsset(asset);
-                  setInputFromAmount(() => 0);
-                  setErrorMessage(() => undefined);
-                  // setQuote(() => undefined);
-                  console.log({ selectedAsset });
-                }}
-              >
-                <div slot="start" style={{ margin: "0 0.25rem 0 0" }}>
-                  <SymbolIcon
-                    assetIconURL={
-                      asset?.symbol === "ETH" ? undefined : asset.thumbnail
-                    }
-                    symbol={asset.symbol}
-                    chainId={asset.chain?.id}
-                    iconSize="28px"
-                  />
-                </div>
-                <IonLabel
-                  style={{
-                    marginLeft: "0.25rem",
-                    lineHeight: "0.8rem",
-                  }}
-                >
-                  <IonText>{asset.symbol}</IonText>
-                  <br />
-                  <IonText color="medium">
-                    <small>
-                      {
-                        CHAIN_AVAILABLES.find((c) => c.id === asset?.chain?.id)
-                          ?.name
-                      }
-                    </small>
-                  </IonText>
-                </IonLabel>
-                <div slot="end" className="ion-text-end">
-                  <IonText>{Number(asset?.balance).toFixed(6)}</IonText>
-                  <br />
-                  <IonText color="medium">
-                    <small>
-                      {getReadableAmount(
-                        +asset?.balance,
-                        Number(asset?.priceUsd),
-                        "No deposit"
-                      )}
-                    </small>
-                  </IonText>
-                </div>
-              </IonItem>
-            ))}
-        </IonList>
-      </IonModal>
-    </>
-  );
-};
-
 export const TransferContainer = (props: { dismiss: () => Promise<void> }) => {
   const { assets, loadAssets, transfer, switchNetwork, currentNetwork } =
     Store.useState(getWeb3State);
@@ -395,8 +172,8 @@ export const TransferContainer = (props: { dismiss: () => Promise<void> }) => {
   );
 
   const isValid =
-    inputFromAmount > 0 &&
-    inputToAddress &&
+    inputFromAmount > 0 && inputFromAmount <= (inputFromAsset?.balance || 0);
+  inputToAddress &&
     inputToAddress.length > 0 &&
     inputFromAsset?.contractAddress;
 
@@ -439,6 +216,7 @@ export const TransferContainer = (props: { dismiss: () => Promise<void> }) => {
       }
     }
   };
+
   return (
     <>
       <IonHeader className="ion-no-border" translucent={true}>
@@ -551,6 +329,22 @@ export const TransferContainer = (props: { dismiss: () => Promise<void> }) => {
               >
                 <IonCol size="12" className="ion-margin-vertical">
                   <CrossIcon message={errorMessage} />
+                </IonCol>
+              </IonRow>
+            </>
+          )}
+
+          {inputFromAmount > (inputFromAsset?.balance || 0) && (
+            <>
+              <IonRow
+                className="ion-text-center ion-align-items-center"
+              >
+                <IonCol size="12">
+                  <WarningBox>
+                    <p>
+                      You don't have enough funds to complete the transaction.
+                    </p>
+                  </WarningBox>
                 </IonCol>
               </IonRow>
             </>
