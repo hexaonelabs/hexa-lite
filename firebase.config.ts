ac99fad6daf912e3dc8c86195279839f158b2294
FazioNico <contact@nicolasfazio.ch>
2024-06-07 16:17:36 +0200

    refactor: prevent multi initialize app

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
