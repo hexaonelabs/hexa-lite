48728dee7b25bff66c42851a3b3eb3baa23a53af
FazioNico <contact@nicolasfazio.ch>
2025-03-25 13:49:43 +0100

    fix: wrapp with missing ion-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import '@khmyznikov/pwa-install';
 
 @Component({
   selector: 'app-root',
-  imports: [IonRouterOutlet],
+  imports: [IonRouterOutlet, IonApp],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
