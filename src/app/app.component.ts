4276c3be593725663931e045b3c5a0a588de36a1
FazioNico <contact@nicolasfazio.ch>
2025-04-01 15:24:55 +0200

    feat: add notif comp for app update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { UpdateNotifComponent } from './components/widgets/update-notif/update-notif.component';
 import '@khmyznikov/pwa-install';
 
 @Component({
   selector: 'app-root',
-  imports: [IonRouterOutlet, IonApp],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [IonRouterOutlet, IonApp, UpdateNotifComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
---
+++ src/app/components/widgets/update-notif/update-notif.component.ts
@@ -0,0 +1,53 @@
+import { AsyncPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { environment } from '@env/environment';
+import { ToastOptions } from '@ionic/angular';
+import { ToastController } from '@ionic/angular/standalone';
+import { Observable, switchMap } from 'rxjs';
+
+@Component({
+  selector: 'app-update-notif',
+  templateUrl: './update-notif.component.html',
+  styleUrls: ['./update-notif.component.scss'],
+  imports: [AsyncPipe],
+})
+export class UpdateNotifComponent  implements OnInit {
+  public readonly updateAvailable$: Observable<void>;
+  constructor(
+    private updates: SwUpdate,
+  ) {
+    this.updateAvailable$ = this.updates.versionUpdates.pipe(
+      switchMap(async (event) => {
+        console.log('event', event);
+        if (event.type === 'VERSION_READY') {
+          await this._displayNotif();
+        }
+      }),
+    );
+  }
+
+  ngOnInit() {}
+
+  async activateUpdate() {
+    if (!environment.isProd) {
+      return;
+    }
+    await this.updates.activateUpdate()
+    location.reload();
+  }
+
+  private async _displayNotif() {
+    const data = <ToastOptions>{
+      message: 'New version available!',
+      position: 'bottom',
+      showCloseButton: true,
+      closeButtonText: `Update`,
+    };
+    const toast = await new ToastController().create(data);
+    await toast.present();
+    await toast.onDidDismiss()
+    await this.activateUpdate()
+  }
+
+}
